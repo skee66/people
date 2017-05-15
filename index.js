63e4ea95c34cb5b6da7278848a9ebb38ad497682
Samuel Roberts <rober352@purdue.edu>
2017-05-15 16:20:02 -0400

    potatoes

--- index.js
+++ index.js
@@ -7,15 +7,32 @@ personForm.onsubmit = (ev) => {
 
     const personName = form.personName.value;
     const hairColor = form.hairColor.value;
+    const age = form.age.value;
+    const birthplace = form.birthplace.value;
 
-    const em = document.createElement('em');
+    const em = document.createElement('li');
     em.textContent = personName;
 
-    const colorDiv = document.createElement('div');
+    const colorDiv = document.createElement('li');
     colorDiv.style.backgroundColor = hairColor;
     colorDiv.style.height = '50px';
     colorDiv.style.wdith = '100px';
-    
-    details.appendChild(em);
-    details.appendChild(colorDiv);
+
+    const ageDiv = document.createElement('li');
+    ageDiv.textContent = age;
+
+    const birthplaceDiv = document.createElement('li');
+    birthplaceDiv.textContent = birthplace;
+
+details.innerHTML = `
+<ul>
+    <li>Name: ${personName}</li>
+    <li>HairColor: ${hairColor.innerHTML}</li>
+    <li>Age: ${age}</li>
+    <li>Birthplace: ${birthplace}</li>
+</ul>`
+    // details.appendChild(em);
+    // details.appendChild(colorDiv);
+    // details.appendChild(ageDiv);
+    // details.appendChild(birthplaceDiv);
 };
