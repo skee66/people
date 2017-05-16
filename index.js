53a451a32cc094e39205d554eb981f2d2af5781c
Samuel Roberts <rober352@purdue.edu>
2017-05-15 22:14:39 -0400

    Add loop-iterating through form elements

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const personForm = document.querySelector('form');
 
 const renderColor = (hairColor) => {
-    const colorDiv = document.createElement('div');
+    const colorDiv = document.createElement('li');
     colorDiv.style.backgroundColor = hairColor;
     colorDiv.style.height = '50px';
-    colorDiv.style.wdith = '100px';
+    colorDiv.style.width = '100px';
     return colorDiv;
 }
 
@@ -18,28 +18,44 @@ personForm.onsubmit = (ev) => {
     const age = form.age.value;
     const birthplace = form.birthplace.value;
 
-    const em = document.createElement('em');
+    const em = document.createElement('li');
     em.textContent = personName;
 
     const colorDiv = renderColor(hairColor);
 
-    const ageDiv = document.createElement('div');
+    const ageDiv = document.createElement('li');
     ageDiv.textContent = age;
 
-    const birthplaceDiv = document.createElement('div');
+    const birthplaceDiv = document.createElement('li');
     birthplaceDiv.textContent = birthplace;
 
-details.innerHTML = `
-<ul>
-    <li>Name: ${personName}</li>
-    <li>Hair Color: ${colorDiv.outerHTML}</li>
-    <li>Age: ${age}</li>
-    <li>Birthplace: ${birthplace}</li>
-</ul>`
+    // details.innerHTML = `
+    // <ul>
+    //     <li>Name: ${personName}</li>
+    //     <li>Hair Color: ${colorDiv.outerHTML}</li>
+    //     <li>Age: ${age}</li>
+    //     <li>Birthplace: ${birthplace}</li>
+    // </ul>`
+
+
     // details.appendChild(em);
     // details.appendChild(colorDiv);
     // details.appendChild(ageDiv);
     // details.appendChild(birthplaceDiv);
+    // console.log(details);
+
+    const elements = form.elements;
+    console.log(elements);
+    let currElement;
+    for (let i = 0; i < elements.length - 1; i++) {
+        if (i == 1) {
+            currElement = renderColor(elements[i].value);
+        } else {
+            currElement = document.createElement('li');
+            currElement.textContent = elements[i].value;
+        }
+        details.appendChild(currElement);
+    }
 };
 
 personForm.addEventListener('submit', handleSubmit);
